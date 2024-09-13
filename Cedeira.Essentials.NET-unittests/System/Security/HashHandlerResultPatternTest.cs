3fed61d91e64590c1203f874d8069bd1917f4bea
MatiasMeierCedeira <matias.meier@cedeira.com.ar>
2024-09-13 13:47:43 -0300

    feat: se sumaron test para los casos de streamReader

--- Cedeira.Essentials.NET-unittests/System/Security/HashHandlerResultPatternTest.cs
+++ Cedeira.Essentials.NET-unittests/System/Security/HashHandlerResultPatternTest.cs
@@ -11,8 +11,11 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
     [TestClass]
     public class HashHandlerResultPatternTest
     {
-        private Dictionary<string, (string inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)> testCasesinputString;
-        private Dictionary<string, (byte[] inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)> testCasesInputByte;
+        private Dictionary<string, (string inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)> _testCasesinputString;
+        private Dictionary<string, (byte[] inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)> _testCasesInputByte;
+        private Dictionary<string, (StreamReader inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)> _testCasesinputStreamReader;
+        private Dictionary<string, (SecureString inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)> _testCasesInputSecureString;
+
 
         private IResultFactory _resultFactory;
         private Func<byte[], string> _hashformatterBase64;
@@ -20,7 +23,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
         private byte[] _inputByte;
         private string _message;
 
-        private StreamReader _inputstreamReader;
+        private StreamReader _inputStreamReader;
         private SecureString _inputSecureString;
 
         [TestInitialize]
@@ -29,7 +32,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
             _input = "Testeo123";
             _message = "Input can not be null";
             _inputByte = Encoding.UTF8.GetBytes(_input);
-            _inputstreamReader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(_input)));
+            _inputStreamReader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(_input)));
             _inputSecureString = new SecureString();
             _resultFactory = new ResultFactory();
 
@@ -45,7 +48,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
         [TestMethod]
         public void CalculateHash_InputString_ReturnsExpectedHash()
         {
-            testCasesinputString = new Dictionary<string, (string inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
+            _testCasesinputString = new Dictionary<string, (string inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
             {
                     {"MD5_1", new (_input,MD5.Create(), true, "320dee96d097dda6f108c62983def31f") },
                     {"SHA256_1", new (_input,SHA256.Create(), true, "167c675e41e07059088728924744805f06dfc328eedf5f1939dd8143d6d78226")},
@@ -56,7 +59,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
                     {"SHA256_Null", new (null, SHA256.Create(), false, _message)},
             };
 
-            foreach (var testCase in testCasesinputString)
+            foreach (var testCase in _testCasesinputString)
             {
                 var handlerInstance = new HashHandlerResultPattern(testCase.Value.algorithm, _resultFactory);
 
@@ -78,7 +81,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
         [TestMethod]
         public void CalculateHash_InputString_ReturnsExpectedHashBase64()
         {
-            testCasesinputString = new Dictionary<string, (string inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
+            _testCasesinputString = new Dictionary<string, (string inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
             {
                     {"MD5_1", new (_input, MD5.Create(), true, "Mg3ultCX3abxCMYpg97zHw==")},
                     {"SHA256_1", new (_input,SHA256.Create(), true, "FnxnXkHgcFkIhyiSR0SAXwbfwyju318ZOd2BQ9bXgiY=")},
@@ -89,7 +92,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
                     {"SHA256_Null", new (null, SHA256.Create(), false, _message)},
             };
 
-            foreach (var testCase in testCasesinputString)
+            foreach (var testCase in _testCasesinputString)
             {
                 var handlerInstance = new HashHandlerResultPattern(testCase.Value.algorithm, _resultFactory, _hashformatterBase64);
 
@@ -111,7 +114,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
         [TestMethod]
         public void CalculateHash_InputByte_ReturnsExpectedHash()
         {
-            testCasesInputByte = new Dictionary<string, (byte[] inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
+            _testCasesInputByte = new Dictionary<string, (byte[] inputName, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
             {
                     {"MD5_1", new (_inputByte, MD5.Create(), true, "320dee96d097dda6f108c62983def31f")},
                     {"SHA256_1", new (_inputByte, SHA256.Create(), true, "167c675e41e07059088728924744805f06dfc328eedf5f1939dd8143d6d78226")},
@@ -122,7 +125,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
                     {"SHA256_Null", new (null, SHA256.Create(), false, _message)},
             };
 
-            foreach (var testCase in testCasesInputByte)
+            foreach (var testCase in _testCasesInputByte)
             {
                 var handlerInstance = new HashHandlerResultPattern(testCase.Value.algorithm, _resultFactory);
 
@@ -144,7 +147,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
         [TestMethod]
         public void CalculateHash_InputByte_ReturnsExpectedHashBase64()
         {
-            testCasesInputByte = new Dictionary<string, (byte[] inputByte, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
+            _testCasesInputByte = new Dictionary<string, (byte[] inputByte, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
             {
                     {"MD5_1", new (_inputByte, MD5.Create(), true, "Mg3ultCX3abxCMYpg97zHw==")},
                     {"SHA256_1", new (_inputByte,SHA256.Create(), true, "FnxnXkHgcFkIhyiSR0SAXwbfwyju318ZOd2BQ9bXgiY=")},
@@ -155,7 +158,7 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
                     {"SHA256_Null", new (null, SHA256.Create(), false,_message)},
             };
 
-            foreach (var testCase in testCasesInputByte)
+            foreach (var testCase in _testCasesInputByte)
             {
                 var handlerInstance = new HashHandlerResultPattern(testCase.Value.algorithm, _resultFactory, _hashformatterBase64);
 
@@ -174,6 +177,77 @@ namespace Cedeira.Essentials.NET_unittests.System.Security
 
             }
         }
+
+
+
+        [TestMethod]
+        public void CalculateHash_InputStreamReader_ReturnsExpectedHash()
+        {
+            _testCasesinputStreamReader = new Dictionary<string, (StreamReader inputStream, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
+            {
+                    {"MD5_1", new (_inputStreamReader, MD5.Create(), true, "320dee96d097dda6f108c62983def31f")},
+                    {"SHA256_1", new (_inputStreamReader, SHA256.Create(), true, "167c675e41e07059088728924744805f06dfc328eedf5f1939dd8143d6d78226")},
+                    {"SHA1_1", new (_inputStreamReader, SHA1.Create(), true, "6e17ffc27e415630eaa5e5297da569573267cd11")},
+                    {"SHA384_1", new (_inputStreamReader, SHA384.Create(), true, "f4c32eff1d108679dd2149c2d48babf350db0be0e0ed08ccc80fc5b037df52f550fd1eb76d3ae3024d1957271ac8d6a1")},
+                    {"SHA512_1", new (_inputStreamReader, SHA512.Create(), true, "aeaca907a9bce24dbf9762049b6afddd6ac124b2720d2a91c3317500c8691442a98230f674bc58b5da4553a510e3eced7141dadc5eb8226836f524cee0feac66")},
+                    {"MD5_Empty", new (new StreamReader(new MemoryStream()), MD5.Create(), true, "d41d8cd98f00b204e9800998ecf8427e")},
+                    {"SHA256_Null", new (null, SHA256.Create(), false, _message)},
+            };
+
+            foreach (var testCase in _testCasesinputStreamReader)
+            {
+                var handlerInstance = new HashHandlerResultPattern(testCase.Value.algorithm, _resultFactory);
+
+                var result = handlerInstance.CalculateHash(testCase.Value.inputName);
+
+                if (testCase.Value.estadoEsperado)
+                {
+                    Assert.IsTrue(result.IsSuccess());
+                    Assert.AreEqual(testCase.Value.hashEsperado, result.SuccessValue);
+                }
+                else
+                {
+                    Assert.IsTrue(result.IsFailure());
+                    Assert.AreEqual(testCase.Value.hashEsperado, result.Message);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CalculateHash_InputStreamReader_ReturnsExpectedHashBase64()
+        {
+            _testCasesinputStreamReader = new Dictionary<string, (StreamReader inputStream, HashAlgorithm algorithm, bool estadoEsperado, string hashEsperado)>
+            {
+                    {"MD5_1", new (_inputStreamReader, MD5.Create(), true, "Mg3ultCX3abxCMYpg97zHw==")},
+                    {"SHA256_1", new (_inputStreamReader,SHA256.Create(), true, "FnxnXkHgcFkIhyiSR0SAXwbfwyju318ZOd2BQ9bXgiY=")},
+                    {"SHA1_1", new (_inputStreamReader,SHA1.Create(), true, "bhf/wn5BVjDqpeUpfaVpVzJnzRE=")},
+                    {"SHA384_1", new (_inputStreamReader,SHA384.Create(), true, "9MMu/x0QhnndIUnC1Iur81DbC+Dg7QjMyA/FsDffUvVQ/R63bTrjAk0ZVycayNah")},
+                    {"SHA512_1", new (_inputStreamReader,SHA512.Create(), true, "rqypB6m84k2/l2IEm2r93WrBJLJyDSqRwzF1AMhpFEKpgjD2dLxYtdpFU6UQ4+ztcUHa3F64Img29STO4P6sZg==")},
+                    {"MD5_Empty", new (new StreamReader(new MemoryStream()), MD5.Create(), true, "1B2M2Y8AsgTpgAmY7PhCfg==")},
+                    {"SHA256_Null", new (null, SHA256.Create(), false,_message)},
+            };
+
+            foreach (var testCase in _testCasesinputStreamReader)
+            {
+                var handlerInstance = new HashHandlerResultPattern(testCase.Value.algorithm, _resultFactory, _hashformatterBase64);
+
+                var result = handlerInstance.CalculateHash(testCase.Value.inputName);
+
+                if (testCase.Value.estadoEsperado)
+                {
+                    Assert.IsTrue(result.IsSuccess());
+                    Assert.AreEqual(testCase.Value.hashEsperado, result.SuccessValue);
+                }
+                else
+                {
+                    Assert.IsTrue(result.IsFailure());
+                    Assert.AreEqual(testCase.Value.hashEsperado, result.Message);
+                }
+
+            }
+
+        }
+
 
 
 
