72b4952bc7d3faa48f1618cd50a59421ed47c4fc
MatiasMeierCedeira <matias.meier@cedeira.com.ar>
2025-01-06 14:25:11 -0300

    feat: se incluyo la implementacion de coalesce en seguimiento con la necesidad planteada en la issue. Se incorporó soporte de collections y continuidad en el caso de excepciones. Se incluyen pruebas de test unitartias

---
+++ Cedeira.Essentials.NET-unittests/System/Collections/UserClassTest.cs
@@ -0,0 +1,7 @@
+﻿namespace Cedeira.Essentials.NET_unittests.System.Collections
+{
+    public class UserClassTest
+    {
+        public string? Name { get; set; }
+    }
+}
---
+++ Cedeira.Essentials.NET/System/Resilience/Fallback/FallbackStrategy.cs
@@ -0,0 +1,89 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Cedeira.Essentials.NET.System.Resilience.Fallback
+{
+    public static class FallbackStrategy
+    {
+        public static T Coalesce<T>(params Func<T>[] valueProviders)
+        {
+
+            foreach (var valueProvider in valueProviders)
+            {
+                try
+                {
+                    var result = valueProvider();
+
+                    if (result is string s)
+                    {
+                        if (!string.IsNullOrWhiteSpace(s))
+                        {
+                            return result;
+                        }
+                        else { continue; }
+                    }
+
+                    if (result is IEnumerable<object> collection)
+                    {
+                        if (collection != null && collection.Any())
+                        {
+                            return result;
+                        }
+                        else { continue; }
+                    }
+
+                    if (!EqualityComparer<T>.Default.Equals(result, default!))
+                    {
+                        return result;
+                    }
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+
+            return default!;
+        }
+
+        public static async Task<T> Coalesce<T>(params Func<Task<T>>[] valueProviders)
+        {
+            foreach (var valueProvider in valueProviders)
+            {
+                try
+                {
+                    var result = await valueProvider();
+
+                    if (result is string s)
+                    {
+                        if (!string.IsNullOrWhiteSpace(s))
+                        {
+                            return result;
+                        }
+                        else { continue; }
+                    }
+
+                    if (result is IEnumerable<object> collection)
+                    {
+                        if (collection != null && collection.Any())
+                        {
+                            return result;
+                        }
+                        else { continue; }
+                    }
+
+                    if (!EqualityComparer<T>.Default.Equals(result, default!))
+                    {
+                        return result;
+                    }
+
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+
+            return default!;
+        }
+    }
+}
