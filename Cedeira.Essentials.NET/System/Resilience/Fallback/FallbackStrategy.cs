8142e72de57ff10aaaa080e822b2322887fbcc74
MatiasMeierCedeira <matias.meier@cedeira.com.ar>
2025-01-06 14:34:44 -0300

    feat: se incluyeron summaries

--- Cedeira.Essentials.NET/System/Resilience/Fallback/FallbackStrategy.cs
+++ Cedeira.Essentials.NET/System/Resilience/Fallback/FallbackStrategy.cs
@@ -2,11 +2,19 @@
 
 namespace Cedeira.Essentials.NET.System.Resilience.Fallback
 {
+    /// <summary>
+    /// class that provides a fallback strategy for multiple value providers.
+    /// </summary>
     public static class FallbackStrategy
     {
+        /// <summary>
+        /// Returns the first non-null value from a list of value providers.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="valueProviders"></param>
+        /// <returns></returns>
         public static T Coalesce<T>(params Func<T>[] valueProviders)
         {
-
             foreach (var valueProvider in valueProviders)
             {
                 try
@@ -45,6 +53,12 @@ namespace Cedeira.Essentials.NET.System.Resilience.Fallback
             return default!;
         }
 
+        /// <summary>
+        /// Returns the first non-null value from a list of value providers.Async version.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="valueProviders"></param>
+        /// <returns></returns>
         public static async Task<T> Coalesce<T>(params Func<Task<T>>[] valueProviders)
         {
             foreach (var valueProvider in valueProviders)
