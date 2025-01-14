3ef5d58aef61a3cb6faeb9b63c5e28972c44b46c
MatiasMeierCedeira <matias.meier@cedeira.com.ar>
2025-01-14 10:30:34 -0300

    feat: su sumaron los summaries

--- Cedeira.Essentials.NET/System/Range/DateRange.cs
+++ Cedeira.Essentials.NET/System/Range/DateRange.cs
@@ -3,18 +3,36 @@
 
 namespace Cedeira.Essentials.NET.System.Range
 {
+    /// <summary>
+    /// this class represents a date range with a start and end date
+    /// </summary>
     public readonly struct DateRange
     {
+        /// <summary>
+        /// start date of the range
+        /// </summary>
         public DateTime Start { get; }
+
+        /// <summary>
+        /// end date of the range
+        /// </summary>
         public DateTime End { get; }
+        /// <summary>
+        /// duration of the range (end - start)
+        /// </summary>
         public TimeSpan Duration => End - Start;
 
+        /// <summary>
+        /// creates a new instance of the <see cref="DateRange"/> class
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <exception cref="ArgumentException"></exception>
         public DateRange(DateTime start, DateTime end)
         {
             Invariants.For(start).IsNotNull();
             Invariants.For(end).IsNotNull();
 
-            //todo:Incluir custom Invariant
             if (start > end)
             {
                 throw new ArgumentException("The start date must be earlier than or equal to the end date.");
@@ -24,19 +42,31 @@ namespace Cedeira.Essentials.NET.System.Range
             End = end;
         }
 
-        //verificar si una fecha específica está dentro del rango
+        /// <summary>
+        /// checks if the date is within the range
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public bool Contains(DateTime date)
         {
             return (date >= Start && date <= End);
         }
 
-        //validar solapamientos.
+        /// <summary>
+        /// checks if the range overlaps with another range
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public bool OverlapsWith(DateRange other)
         {
             return (Start < other.End && End > other.Start);
         }
 
-        // Devuelve el rango comun entre dos rangos  
+        /// <summary>
+        /// return the intersection of the range with another range
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public DateRange? Intersect(DateRange other)
         {
             if (!OverlapsWith(other))
@@ -50,22 +80,48 @@ namespace Cedeira.Essentials.NET.System.Range
             return new DateRange(intersectionStart, intersectionEnd);
         }
 
+        /// <summary>
+        /// returns the hash code of the range.
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode() => HashCode.Combine(Start, End);
 
+        /// <summary>
+        /// returns true if the range is equal to another range
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator ==(DateRange left, DateRange right)
         {
             return left.Equals(right);
         }
+
+        /// <summary>
+        /// returns true if the range is not equal to another range
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static bool operator !=(DateRange left, DateRange right)
         {
             return !(left == right);
         }
 
+        /// <summary>
+        /// returns a string representation of the range.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{Start:yyyy-MM-dd} to {End:yyyy-MM-dd}";
         }
 
+        /// <summary>
+        /// used to compare the range with another object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object? obj)
         {
             return (obj is DateRange other && Start == other.Start && End == other.End);
